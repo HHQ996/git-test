9d392e3708a957418f617b0a030843463d686863
HHq <1615760895@qq.com>
2022-02-27 11:36:02 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-02-27 11:32
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
